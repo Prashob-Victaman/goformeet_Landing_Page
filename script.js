277471fe6ac647e5ebd0b2ac94c057ae40414889
Phani <phani@goformeet.co>
2024-03-08 09:03:08 +0530

    Removed Mobile Phone number view

--- script.js
+++ script.js
@@ -87,18 +87,21 @@ overlay.addEventListener("click", () => {
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", async () => {
-  const mobileNumber = "+91" + document.getElementById("mobileNumber").value;
+  const number = document.getElementById("mobileNumber").value;
+  const mobileNumber = "+91" + number;
+  console.log(number);
   const apiUrl = `https://enterprise.smsgupshup.com/GatewayAPI/rest?userid=2000236219&password=JWLEzyyqC&send_to=${mobileNumber}&method=SendMessage&msg=Dear%20Customer,%3Cbr%3E%3Cbr%3E%20Your%20friend%20Goformeet,%20has%20invited%20you%20to%20download%20Goformeet.%20Download%20https://onelink.to/zyv2v2%3Cbr%3E%20Goformeet &msg_type=TEXT&&auth_scheme=plain&v=1.1&format=text`;
   try {
     const response = await fetch(apiUrl, { method: "GET" });
-    console.log(response)
+    console.log(response);
     if (response.ok) {
-      alert("Sent successfully! check your mobile")
-      document.getElementById("mobileNumber").value = "";
+      alert("Sent successfully! check your mobile");
     } else {
-      alert("Request Failed Try again Later")
+      alert("Request Failed Try again Later");
     }
   } catch (error) {
     console.log(error.message);
+  } finally {
+    document.getElementById("mobileNumber").value = "";
   }
 });
